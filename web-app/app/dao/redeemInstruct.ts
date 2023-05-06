35f46c5d25e661218020a675d2cc1f647f0bfa89
farauj0 <faraujo0080@gmail.com>
2023-05-06 17:27:10 -0300

    feat: redeem Instructions

--- web-app/app/dao/redeemInstruct.ts
+++ web-app/app/dao/redeemInstruct.ts
@@ -1,23 +1,23 @@
 import dotenv from "dotenv";
 import path from "path";
 import axios from "axios";
-import ListGiftsDto from "~/dto/listGifts.dto";
+import RedeemInstructionsDto from "~/dto/redeemInstruct.dto";
 
 // Load credentials from .env file
 
 const envPath = path.resolve(__dirname, "../.env");
 dotenv.config({ path: envPath });
-const giftlistURL = process.env.GIFT_LIST_URL;
+const redeemUrl = process.env.REDEEM_URL;
 const access_token = process.env.ACCESS_TOKEN;
 
-export default async function listGifts(dto: ListGiftsDto): Promise<any> {
+export default async function listGifts(dto: RedeemInstructionsDto): Promise<any> {
   const headers = {
     "Content-Type": "application/json",
     Accept: "application/json",
     Authorization: `Bearer ${access_token}`,
   };
 
-  const response = await axios.post(giftlistURL as string, dto, {
+  const response = await axios.post(redeemUrl as string, dto, {
     headers: headers,
   });
 
--- web-app/app/dto/listGifts.dto.ts
+++ web-app/app/dto/listGifts.dto.ts
@@ -1,13 +1,5 @@
-export default interface CreateOrderDto {
-    productId: number,
-    countryCode?: string,
+export default interface ListGiftsDto {
+    size: number,
+    productName: string,
     quantity: number,
-    unitPrice: number,
-    customIdentifier?: string,
-    senderName: string,
-    recipientEmail: string,
-    recipientPhoneDetails?: {
-        countryCode: string, 
-        phoneNumber: string
-    }
 }
