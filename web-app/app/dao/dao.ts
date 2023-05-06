c5c792d9dfa8ec4f8fc953a76e70f77a32aec646
joao.rosa <joaojunior656@hotmail>
2023-05-06 16:17:25 -0300

    feat: adding axios post req for orders

--- web-app/app/dao/dao.ts
+++ web-app/app/dao/dao.ts
@@ -1,59 +1,28 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 import path from 'path';
+import axios from 'axios'
 import CreateOrderDto from '~/dto/createOrder.dto';
 
 // Load credentials from .env file
 
 const envPath = path.resolve(__dirname, '../.env');
 dotenv.config({ path: envPath });
-const clientId = process.env.RELOADLY_CLIENT_ID!;
-const clientSecret = process.env.RELOADLY_CLIENT_SECRET!;
-const url = process.env.TOKEN_URL
+const orderUrl = process.env.ORDER_URL
+const access_token = process.env.ACCESS_TOKEN
 
 
-export default function createOrder(dto: CreateOrderDto) {
-  const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    body: JSON.stringify({
-      client_id: clientId,
-      client_secret: clientSecret,
-      grant_type: 'client_credentials',
-      audience: 'https://giftcards-sandbox.reloadly.com',
-    }),
-  };
-  
-  fetch(url as string, options)
-    .then((res) => res.json())
-    .then((json: any) => {
-      const accessToken = json.access_token;
-      // Set up headers for authorized requests
-      const headers = {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${json.access_token}`,
-      };      
-  
-      // Example: Get gift cards
-  
-      const url = 'https://giftcards-sandbox.reloadly.com/orders';
-  
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/com.reloadly.giftcards-v1+json',
-          Authorization: `Bearer ${json.access_token}`
-        },
-        body: JSON.stringify(dto)
-      };
-  
-      fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('error:' + err));
-    })
-    .catch((err) => console.error('error:' + err));
-  
+export default async function createOrder(dto: CreateOrderDto): Promise<any> {
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${access_token}`,
+  };     
+
+  const response = await axios.post(orderUrl as string, dto, {
+    headers: headers
+  });
+
+  return response
 }
