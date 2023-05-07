e4b875f0f271ece6c9a1dbe8848691f5dc1a01f0
joao.rosa <joaojunior656@hotmail>
2023-05-07 09:56:18 -0300

    feat: style on the display info

--- web-app/app/routes/components/giftCard.tsx
+++ web-app/app/routes/components/giftCard.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface GiftCardProps {
   productId: number;
   productName: string;
@@ -23,38 +25,34 @@ const GiftCardComponent: React.FC<GiftCardProps> = ({
 }) => {
   let showFixed = false;
   if (!productId) return <div />;
-  if  (denomination == "FIXED") {
-      showFixed = true;
+  if (denomination === "FIXED") {
+    showFixed = true;
   }
   return (
-    <div className="flex flex-col items-center justify-center relative">
-
-      <div
-        id="partnerCard"
-        className="bg-[#ffffff] text-white-50 overflow-hidden rounded-md  p-2 min-h-[150px] flex flex-col"
-      >
+    <div className="flex items-center justify-center relative">
+      <div className="bg-gray-100 text-white-50 overflow-hidden rounded-md p-2 min-h-[150px] flex flex-col items-center justify-center mt-4">
         <div>
-          <h3 className="text-left pl-8 pb-4 pt-2 text-xl">
-            {productName}
-          </h3>
+          <h3 className="text-left pl-8 pb-4 pt-2 text-xl">{productName}</h3>
         </div>
 
         <div className="flex items-center justify-center bg-[#ffffff] min-h-[100px]">
-
-          <img
-            src={logoUrls[0]}
-            alt="EasyCode"
-            className="w-1/2 object-cover"
-          />
-
+          <img src={logoUrls[0]} alt="EasyCode" className="w-1/2 object-cover" />
         </div>
-        <div className="grid grid-cols-4">
+
+        <div className="grid grid-cols-4 w-full">
           <div className="p-4 pr-0 text-lg col-span-3">
-            <p>
-              Brand: {brand}
-            </p>
-           {!showFixed && <p>Range of Values:{currency} {maxRecipientDenomination} - {minRecipientDenomination}</p>}
-           {showFixed && <p>Value:{currency} {fixedRecipientDenominations[0]}</p>}
+            <p>Brand: {brand}</p>
+            {!showFixed && (
+              <p>
+                Range of Values: {currency} {maxRecipientDenomination} -{" "}
+                {minRecipientDenomination}
+              </p>
+            )}
+            {showFixed && (
+              <p>
+                Value: {currency} {fixedRecipientDenominations[0]}
+              </p>
+            )}
           </div>
         </div>
       </div>
