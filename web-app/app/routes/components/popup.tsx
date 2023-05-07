6afccc8a6c253d929f9ae709510ec7f9508a1343
joao.rosa <joaojunior656@hotmail>
2023-05-07 09:50:25 -0300

    feat: popup

--- web-app/app/routes/components/popup.tsx
+++ web-app/app/routes/components/popup.tsx
@@ -1,40 +1,48 @@
-import React from "react";
-import {
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-  Button,
-} from "@material-tailwind/react";
+import React, { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
+import { Button } from "@material-tailwind/react";
+import { Link } from "@remix-run/react";
 
 interface PopupProps {
   content: any;
 }
 
 const PopupComponent = ({ content }: PopupProps) => {
-    console.log(content)
-    if(content.status == 200) {
-        return (
-            <Popover>
-                <PopoverHandler>
-                <Button>Show information</Button>
-                </PopoverHandler>
-                <PopoverContent>
-                    <h4>{content.message}</h4>
-                    <p>E-mail: {content.email}</p>
-                    <p>Phone: {content.phone}</p>
-                    <p>Transaction ID: {content.transactionId}</p>
-                </PopoverContent>
-            </Popover>
-            );
-    }
-    return (
-    <Popover>
-        <PopoverHandler>
-        <Button>Show Popover</Button>
-        </PopoverHandler>
-        <PopoverContent>{content.message}</PopoverContent>
-    </Popover>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      {isOpen &&
+        createPortal(
+          <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white rounded-lg px-8 py-6 shadow-md max-w-md w-full">
+              {content.status === 200 ? (
+                <>
+                  <h4 className="text-lg font-bold mb-2">{content.message}</h4>
+                  <p className="text-gray-700 mb-2">E-mail: {content.email}</p>
+                  <p className="text-gray-700 mb-2">Phone: {content.phone}</p>
+                  <p className="text-gray-700 mb-2">Transaction ID: {content.transactionId}</p>
+                </>
+              ) : (
+                <>{content.message}</>
+              )}
+              <Button>
+                <Link to="/">Close</Link>
+              </Button>
+            </div>
+          </div>,
+          document.body
+        )}
+    </>
+  );
 };
 
 export default PopupComponent;
